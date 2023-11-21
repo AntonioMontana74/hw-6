d60173ed36b9a1f66895cb2d36f2fa4076c3f04f
AntonioMontana74 <149238712+AntonioMontana74@users.noreply.github.com>
2023-11-21 15:40:48 +0300

    Add files via upload

---
+++ Less 4/js/main.js
@@ -0,0 +1,105 @@
+let arr = [1, 2, 3, 4, 5];
+
+// 1 -------------------
+
+// arr.forEach((item) => {
+//     result = item * 2;
+//     console.log(result);
+// } );
+
+// 2 ----------------
+
+// console.log(arr.filter((item)=>{
+//     return item % 2 == 0
+// }));
+
+// for (var i = 0; i < arr.length; i++)
+// {
+//   if ((arr[i] % 2) === 0)
+//   {
+//   	console.log(arr[i]);
+//   }
+// }
+
+
+// 3 ------------------
+
+// console.log(arr.map((item) => {
+//     return item * 3
+
+// }));
+
+// console.log(arr2);
+    
+
+
+
+
+
+// let arr = ['Momush', 'Teshebay', 'Baltabay', 'Sadyrbai'];
+
+
+// let arr2 = arr.map((item)=>{
+//     if(item.length < 5){
+//         return item + 'Hello world'
+//     }
+//     else {
+//         return item
+//     }
+// });
+
+
+
+// console.log(arr2);
+
+
+
+// for (let item of arr){
+//     console.log(item);
+// }
+
+// arr.forEach((item, idx, array)=>{
+//     console.log(array);
+// });
+
+
+// console.log(arr);
+
+
+
+//console.log(arr.includes('Momush'));
+// console.log(arr.lastIndexOf('Momush'));
+// console.log(arr.indexOf('Momush'));
+
+//arr.unshift('Baltabay', 'Teshebay');
+// arr.splice(1, 0, 'Baltabay', 'Teshebay')
+// arr.shift();
+// arr.pop();
+// arr.push('Nurtilek', 'Ayana');
+
+// arr[0] = 'KaraTilek';
+// arr[4] = 'TilekAk';
+// arr[10] = 'Tima';
+
+// console.log(arr.length);
+
+// let key = 'qwerty'
+// let obj = {
+//     name: 'Nurislam',
+//     age: 14,
+// };
+// console.log(obj[key]);
+
+
+// for(let key in obj){
+//     console.log([key]);
+// }
+
+// console.log(Object.keys[obj]);
+// console.log(Object.values[obj]);
+// console.log(Object.entries[obj]);
+
+// Object.freeze(obj);
+// Object.seal(obj)
+// delete obj.name
+// Object.assign(obj2, obj)
